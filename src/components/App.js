7ede42f2da922ac49952dc3c9d2caa996f98dde4
Ana Martinez Fernandez <anamartinezf6@gmail.com>
2020-11-23 17:10:41 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../stylesheet/index.scss";
+
+class App extends React.Component {
+  
+  render() {
+    
+    return (
+      <div className="App">
+        <p>hola</p>
+        
+      </div>
+    );
+  }
+}
+export default App;
